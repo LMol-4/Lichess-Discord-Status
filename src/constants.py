112678ae9976959e2c78c07b4345bb02054776de
Luke Molony <molonyluke@gmail.com>
2025-05-11 23:20:43 +0100

    Added lichess api interaction & Constants

---
+++ src/constants.py
@@ -0,0 +1,20 @@
+# Lichess api
+LICHESS_API_URL = 'https://lichess.org/api'
+
+# Discord presence info
+DISCORD_APP_ID = "1371224988111208458"
+IMAGE = 'lichess-logo'
+DISCORD_RPC_INTERVAL = 20
+
+# status & deatils
+INGAME = 'in a match'
+ONLINE_STATE = 'online'
+LICHESS_DETAILS = 'on lichess.org'
+
+# config path
+CONFIG_FILE_PATH = "config.json"
+
+# game status
+OFFLINE = 0
+ONLINE = 1
+PLAYING = 2
---
+++ src/lichess.py
@@ -0,0 +1,67 @@
+import requests
+
+from constants import (INGAME, LICHESS_API_URL, LICHESS_DETAILS, IMAGE, OFFLINE, ONLINE, ONLINE_STATE, PLAYING)
+
+class Lichess():
+
+    def __init__(self, username):
+        self.username = username
+        self.user_data = {}
+
+    # api request structure
+    def fetch_api_data(url):
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()
+
+        return None
+    
+    # get data from lichess api
+    def update_data(self):
+        user_profile = self.fetch_api_data(f'{LICHESS_API_URL}/user/{self.username}')
+        user_status = self.fetch_api_data(f'{LICHESS_API_URL}/users/status?ids={self.username}')[0] # response is nested in array
+
+        if user_profile is None or user_status is None:
+            return {}
+        
+        self.user_data = {**user_status, **user_profile} # both variable amounts of keys as lots of data retured
+
+    # update rich presence info
+    def display_playing(self, rpc):
+        profile_url = self.user_data['url']
+
+        buttons = [
+            {
+                'label': 'Profile',
+                'url': profile_url,
+            }
+        ]
+
+        rpc.update(details=LICHESS_DETAILS, state=INGAME, large_image=IMAGE, buttons=buttons)
+
+    # update rich presence info
+    def display_online(self, rpc):
+        profile_url = self.user_data['url']
+
+        buttons = [
+            {
+                'label': 'Profile',
+                'url': profile_url,
+            }
+        ]
+
+        rpc.update(details=LICHESS_DETAILS, state=ONLINE_STATE, large_image=IMAGE, buttons=buttons)
+
+    # set player status
+    def get_player_status(self):
+        if ('playing' in self.user_data):
+            return PLAYING
+        if ('online' in self.user_data and self.user_data['online']):
+            return ONLINE
+        return OFFLINE
